95b85cbd59e2e978a96a7018e074f0a71615b4ed
Pomichal <vajk.pomichal@gmail.com>
2021-02-25 18:13:56 +0100

    ADD: project after the second class

--- Assets/ClickerScripts/GameData.cs
+++ Assets/ClickerScripts/GameData.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
+    [Header("Upgrade params")]
     public int upgradeParam1;
     public int upgradeParam2;
 
+    [Header("Score increase param")]
+    [Range(1.2f, 5)]
     public float scoreIncreaseParam;
+
+    [Header("Autoclicker params")]
+    public float autoclickerInterval = 1f;
+    public int autoclickerParam1;
+    public int autoclickerParam2;
 }
--- Assets/ClickerScripts/PlayerBehaviour.cs
+++ Assets/ClickerScripts/PlayerBehaviour.cs
@@ -6,26 +6,62 @@ using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [Header("Game data")]
     public int score;
-    public int scoreIncrease;
+    [SerializeField]
+    private int scoreIncrease;
     public int clickerLevel;
 
+    [Space(15)]
+    [Tooltip("Game Data file with actual game settings")]
     public GameData gameData;
 
+    [Header("Autoclicker data")]
+    public int autoclickerLevel;
+    public int autoclickerCount;
+    public int AutoclickerIncrease;
+
+    [Header("UI elements")]
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI autoClickerCountText;
     public Button upgradeClickerButton;
+    public Button buyAutoclickerButton;
+    public Slider upgradeSlider;
+    public Slider buyAutoclickerSlider;
+
+    public float timer;
 
     void Start()
     {
         score = 0;
         scoreIncrease = 1;
         clickerLevel = 0;
+        autoclickerLevel = 0;
+        AutoclickerIncrease = 1;
         OnScoreUpdate();
+        ChangeAutoClickerCountText();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= gameData.autoclickerInterval)
+        {
+            CollectAutoclickerScore();
+            timer = 0;
+        }
     }
 
     public void CollectSCore()
     {
         score += scoreIncrease;
+        OnScoreUpdate();
+    }
+
+    public void CollectAutoclickerScore()
+    {
+        score += autoclickerCount * AutoclickerIncrease;
         OnScoreUpdate();
     }
 
@@ -45,16 +81,61 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
+    public void UpgradeAutoclicker()
+    {
+        int cost = GetAutoClickerUpgradePrice();
+        if(score >= cost)
+        {
+            autoclickerCount++;
+            ChangeAutoClickerCountText();
+            score -= cost;
+            OnScoreUpdate();
+        }
+        else
+        {
+            Debug.Log("not enough coins for autoclicker (" + cost + ")");
+        }
+    }
+
     public int GetUpgradePrice()
     {
         return (int)(gameData.upgradeParam1 * Mathf.Pow(clickerLevel + 1, gameData.upgradeParam2));
     }
 
+    public int GetAutoClickerUpgradePrice()
+    {
+        return (int)(gameData.autoclickerParam1 * Mathf.Pow(autoclickerCount + 1, gameData.autoclickerParam2));
+    }
+
+    public void ChangeUpgradeSlider(int coins, float maxValue)
+    {
+        //upgradeSlider.value = coins / maxValue;
+
+        upgradeSlider.maxValue = maxValue;
+        upgradeSlider.value = coins;
+    }
+
+    public void ChangeAutoclickerCountSlider(int coins, float maxValue)
+    {
+        buyAutoclickerSlider.value = coins / maxValue;
+    }
+
+    public void ChangeAutoClickerCountText()
+    {
+        autoClickerCountText.text = "Autoclickers: " + autoclickerCount;
+    }
+
     public void OnScoreUpdate()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Score: <size=10>" + score;
         int upgradeCost = GetUpgradePrice();
+        int autoclickerCost = GetAutoClickerUpgradePrice();
 
         upgradeClickerButton.interactable = score >= upgradeCost;
+        buyAutoclickerButton.interactable = score >= autoclickerCost;
+
+
+        ChangeUpgradeSlider(score, upgradeCost);
+        ChangeAutoclickerCountSlider(score, autoclickerCost);
     }
 }
